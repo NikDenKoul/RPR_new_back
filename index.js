73070fd52bbc945a6befa8e80b6b25d7a9a50a5f
centurion <niksoup@list.ru>
2022-05-15 20:37:06 +0300

    В GameMechanics.js вынесены функции взаимодействия с уровнями и аттрибутами

--- index.js
+++ index.js
@@ -816,47 +816,19 @@ app.put("/attributes",
     GameMechanics.editAttributes
 )
 
-app.delete("/attributes",
-    query("id").isInt(),
-    ValidatingFunctions.verifyFields,
-    ValidatingFunctions.verifyToken,
-    async function (req,res){
-        // Получение Id сервера, на котором удаляется аттрибут из набора
-        let [serverId] = await dbP.execute("SELECT server_id FROM attribute WHERE id=?;",[req.query.id]);
-        serverId = serverId[0].server_id;
-
-        // Проверка прав доступа удаления аттрибута
-        let permission = await ValidatingFunctions.checkRights("server_edit",req.userId,serverId);
-        if(!permission){
-            res.status(403).send({error:"access denied"});
-            return;
-        }
-
-        // Удаление аттрибута из набора
-        await dbP.execute("DELETE FROM attribute WHERE id=?",[req.query.id]);
-        res.send({success:1});
-    }
-)
-
 app.get("/profile_attributes",
     query("serverId").isInt(),
     ValidatingFunctions.verifyFields,
     ValidatingFunctions.verifyToken,
-    async function (req,res){
-    let [data] = await dbP.execute("SELECT `attribute`.* FROM attribute WHERE attribute.server_id = ? AND  isGeneral = 1;",
-        [req.query.serverId]);
-    res.send({data:data});
-})
+    GameMechanics.getProfileAttributes
+)
 
 app.get("/all_attributes",
     query("serverId").isInt(),
     ValidatingFunctions.verifyFields,
     ValidatingFunctions.verifyToken,
-    async function (req,res){
-    let [data] = await dbP.execute("SELECT `attribute`.* FROM attribute WHERE attribute.server_id = ?",
-        [req.query.serverId]);
-    res.send({data:data});
-})
+    GameMechanics.getAttributes
+)
 
 /** ========== Взаимодействие с системой опыта ========== */
 
@@ -874,50 +846,8 @@ app.put("/levels",
     body("deletedIds").toArray().isArray(),
     ValidatingFunctions.verifyFields,
     ValidatingFunctions.verifyToken,
-    async function(req,res){
-        let levels = JSON.parse(req.body.levels);
-        let deletedIds = JSON.parse(req.body.deletedIds);
-        let serverId = req.body.serverId;
-
-        // Проверка прав доступа для редактирования набора аттрибутов
-        let permission = await ValidatingFunctions.checkRights("server_edit",req.userId,serverId);
-        if(!permission){
-            res.status(403).send({error:"access denied"});
-            return;
-        }
-
-        // Получаем аттрибуты на сервере
-        let [attributes] = await dbP.execute("SELECT `id`, `name` FROM attribute WHERE server_id=? AND isGeneral=FALSE",[serverId]);
-
-        // Изменение набора уровней на сервере
-        levels.forEach((level, order) => {
-                // Добавление нового уровня (т. к. id'шник был нулевой, присваивается новый)
-                if (level.id <= 0) {
-                    db.query("INSERT INTO level VALUES (NULL, ?, ?, ?);",[level.num, level.required_exp, serverId],async function(err,data){
-                        let lvl_id = data.insertId
-                        attributes.forEach((attribute) => {
-                            dbP.execute("INSERT INTO levels_attributes VALUES (NULL,?,?,?);",[lvl_id,attribute.id,level[attribute.name]]);
-                        })
-                    });
-                }
-                // Обновление существующего уровня
-                else {
-                    dbP.execute("UPDATE level SET num=?, required_exp=?, server_id=? WHERE id=?;",
-                        [level.num, level.required_exp, serverId, level.id]);
-                    attributes.forEach((attribute) => {
-                        dbP.execute("UPDATE levels_attributes SET value=? WHERE lvl_id=? AND attribute_id=?;",[level[attribute.name],level.id,attribute.id]);
-                    })
-                }
-            }
-        )
-
-        // Удаление аттрибутов
-        deletedIds.forEach((lvl_id) => {
-            dbP.execute("DELETE FROM level WHERE id=?",[lvl_id]);
-        })
-
-        res.send({success:1});
-    })
+    GameMechanics.editLevels
+)
 
 /**
  * Получить значения всех аттрибутов по уровням
@@ -926,19 +856,8 @@ app.get("/levels",
     query("serverId").isInt(),
     ValidatingFunctions.verifyFields,
     ValidatingFunctions.verifyToken,
-    async function(req,res){
-    let [levels] = await dbP.execute("SELECT * FROM `level` WHERE server_id=?;",[req.query.serverId]);
-
-    let [levels_attributes] = await dbP.execute("SELECT `levels_attributes`.id, lvl_id, `level`.num, " +
-                                                    "attribute.`name`, `value` FROM `levels_attributes` " +
-                                                    "LEFT JOIN `level` ON lvl_id=`level`.id " +
-                                                    "LEFT JOIN attribute ON attribute_id=attribute.id " +
-                                                    "WHERE `level`.server_id = ? " +
-                                                    "ORDER BY `level`.num;",
-        [req.query.serverId]);
-
-    res.send({levels:levels, levels_attributes:levels_attributes});
-})
+    GameMechanics.getLevels
+)
 
 /** ========== Взаимодействие с игровыми персонажами ========== */
 
